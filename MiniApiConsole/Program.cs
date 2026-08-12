e5db042f3cbdb72139b90ceeba7a106446fbe1ed
owenwilson <owenwilson_m_f@hotmail.com>
2026-08-12 19:58:42 -0400

    feat: create endpoint hello

--- MiniApiConsole/Program.cs
+++ MiniApiConsole/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// endpoint
+app.MapGet("/", () => "Hello from my transformed API");
+
+// get
+app.MapGet("/hello/{name}", (string name) => $"Hi, {name}!");
+//Console.WriteLine("Hello, World!");
+
+app.Run();
