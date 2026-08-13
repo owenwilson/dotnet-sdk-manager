2ff7fed020c28f4691fdc0ea81ed1bbd8549ca7a
owenwilson <owenwilson_m_f@hotmail.com>
2026-08-13 02:14:51 -0400

    feat: add mvc dependencie and add class unitTest1

--- MiniSln/tests/MiniApiConsole.Tests/UnitTest1.cs
+++ MiniSln/tests/MiniApiConsole.Tests/UnitTest1.cs
@@ -1,10 +1,27 @@
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
 namespace MiniApiConsole.Tests;
 
-public class UnitTest1
+public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
 {
-    [Fact]
-    public void Test1()
-    {
+    private readonly HttpClient _client;
 
+    public UnitTest1(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Get_ReturnsHello()
+    {
+        var request = "/";
+        var response = await _client.GetAsync(request);
+
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Hello from my transformed API", responseString);
     }
 }
